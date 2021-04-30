a0e8a2336a70a35c06208f1b1d16084e484b9378
LaoZhenyu <30451660+OXygenPanda@users.noreply.github.com>
2021-04-30 13:56:33 +0800

    v0.1

--- site.config.js
+++ site.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '2d641decc6d94beca171dc2f2e16900e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '氧气的日常',
+  //domain: 'transitivebullsh.it',
+  domain: 'https://www.notion.so/C-2d641decc6d94beca171dc2f2e16900e',
+  author: 'OXygen',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +18,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: '',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
