dd3f6d567cb205bb861b244c5061f3581223ef39
OXygenPanda <laozhenyu961112@gmail.com>
2021-04-30 23:13:35 +0800

    v0.1

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '2d641decc6d94beca171dc2f2e16900e',
-
+  //rootNotionPageId: '2d641decc6d94beca171dc2f2e16900e',
+  rootNotionPageId: '1094e6c6251045988fab426160e54b44',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
@@ -9,7 +9,8 @@ module.exports = {
   // basic site info (required)
   name: '氧气的日常',
   //domain: 'transitivebullsh.it',
-  domain: 'https://www.notion.so/C-2d641decc6d94beca171dc2f2e16900e',
+  //domain: 'https://www.notion.so/C-2d641decc6d94beca171dc2f2e16900e',
+  domain: 'https://www.notion.so/OXygen-Sphere-1094e6c6251045988fab426160e54b44',
   author: 'OXygen',
 
   // open graph metadata (optional)
